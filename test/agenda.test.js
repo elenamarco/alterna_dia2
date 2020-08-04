12accc5c9fa53e5a4e11828bc1d12ddf77b9a54c
Elena <elenamarco96@gmail.com>
2020-08-04 10:27:09 +0200

    funciones agenda mysql

--- test/agenda.test.js
+++ test/agenda.test.js
@@ -2,120 +2,126 @@ const {addUser, saveUser, readUser, addActivity, editUser,deleteUser, listUsers,
 const db = require("../lib/db.js")
 const path = require('path')
 
-describe ("addUser function",()=>{
-    const newUser= ["username","Elena", "Marco", "elenamarco96@gmail.com", "610976608"]
-    const newActivity = ["work", "monday", "9:00"]
-    const newActivity2 = ["home", "tuesday", "18:00"]
-    function expectedUser(newUser){
-        const expUser = addUser(...newUser)
-        expect(expUser).toEqual({username: "username", name:"Elena", lastName:"Marco", email: "elenamarco96@gmail.com", phone: "610976608"})
-    }
-    it("works creating new function",()=>{
-        user = expectedUser(newUser)
+// describe ("addUser function",()=>{
+//     const newUser= ["username","Elena", "Marco", "elenamarco96@gmail.com", "610976608"]
+//     const newActivity = ["work", "monday", "9:00"]
+//     const newActivity2 = ["home", "tuesday", "18:00"]
+//     function expectedUser(newUser){
+//         const expUser = addUser(...newUser)
+//         expect(expUser).toEqual({username: "username", name:"Elena", lastName:"Marco", email: "elenamarco96@gmail.com", phone: "610976608"})
+//     }
+//     it("works creating new function",()=>{
+//         user = expectedUser(newUser)
  
-    })
-})
+//     })
+// })
 
-describe ("Save user function",()=>{
-    const newUser= ["ElenaMarco2", "Elena", "Marco", "email", "phone"]
-    const path1 = path.resolve(__dirname, '../lib/agenda.json')
-    it ("writes new user",()=>{
-        const data = saveUser(newUser, path1)
-        const loadedData = db.load(path1)
-        expect(loadedData[newUser[0]].username).toEqual("ElenaMarco2")
-    })
-
-})
-describe ("Read user function",()=>{
-    const username = "ElenaMarco"
-    const path1 = path.resolve(__dirname, '../lib/agenda.json')
-    it ("reads info from existing user", ()=>{
-        const data = readUser(username, path1)
-        expect(data.username).toEqual("ElenaMarco")
-
-    }) 
-
-})
-// describe ("Add activity function",()=>{
-//     const newActivity = ["ElenaMarco", "work", "monday", "9:00"]
-//     const newActivity2 = ["ElenaMarco","home", "tuesday", "18:00"]
+// describe ("Save user function",()=>{
+//     const newUser= ["ElenaMarco3", "Elena", "Marco", "email", "phone"]
 //     const path1 = path.resolve(__dirname, '../lib/agenda.json')
-//     const expUserAct = addActivity(newActivity,path1)  
-//     it ("works adding activity",()=>{    
-//         const loadedData = db.load(path1)   
-//         expect(loadedData).toEqual({ElenaMarco:{username:"ElenaMarco", name:"Elena", lastName:"Marco", email: "email", phone: "phone", activityList: ["work", "monday", "9:00"]}})
-        
-//     })
-
-//     it ("works when the activity already exists",()=>{
-//         const expNewAct = addActivity(newActivity, path1)
+//     it ("writes new user",()=>{
+//         const data = saveUser(newUser, path1)
 //         const loadedData = db.load(path1)
-//         expect(loadedData).toEqual({ElenaMarco:{username: "ElenaMarco",name:"Elena", lastName:"Marco", email: "email", phone: "phone", activityList: ["work", "monday", "9:00"]}})
-
+//         expect(loadedData[newUser[0]].username).toEqual("ElenaMarco3")
 //     })
-//     it ("works when adding another activity",()=>{
-//         const expnewAct = addActivity(newActivity2, path1)
-//         const loadedData = db.load(path1)
-//         expect(loadedData[newActivity[0]].activityList).toEqual(["work", "monday", "9:00","home", "tuesday", "18:00"])
+//     it ("works when user is duplicated", ()=>{
+//         newUser2 = ["ElenaMarco", "Elena", "Marco", "email", "phone"]
+//         expect(()=>{
+//             saveUser(newUser2,path1)
+//         }).toThrow("Username already taken")
 //     })
 
 // })
-describe ("Edit user function", ()=>{
-    const editedUser = ["ElenaMarco", "Elena2", "Marco", "email", "phone"]
-    const editedUser2 = ["ElenaMarco2", "Elena2", "Marco", "email", "phone"]
-    const path1 = path.resolve(__dirname, '../lib/agenda.json')
-    console.log(path1)
-    const loadedData = db.load(path1)
-    it ("works modifying one element when there is an activity", ()=>{
-        newuser = editUser(editedUser, path1)
-        expect(loadedData[editedUser[0]].name).toEqual("Elena2")
-            
-    })
-    it("works modifying one element when there is no activity",()=>{
-        newuser = editUser(editedUser2, path1)
-        expect(loadedData[editedUser2[0]]).toEqual({"username":"ElenaMarco2","name":"Elena2","lastName":"Marco","email":"email","phone":"phone"})
-    })
-})
-describe("Delete user function",()=>{
-    const username = "ElenaMarco3"
-    const path1 = path.resolve(__dirname, '../lib/agenda.json')
-    
-    const loadedData = db.load(path1)
-    
-    it("works eliminating user",()=>{
-        deleteUser(username,path1)
-        expect(loadedData[username]).toEqual(undefined)
+// describe ("Read user function",()=>{
+//     const username = "ElenaMarco"
+//     const path1 = path.resolve(__dirname, '../lib/agenda.json')
+//     it ("reads info from existing user", ()=>{
+//         const data = readUser(username, path1)
+//         expect(data.username).toEqual("ElenaMarco")
+
+//     }) 
+
+// })
+// // describe ("Add activity function",()=>{
+// //     const newActivity = ["ElenaMarco", "work", "monday", "9:00"]
+// //     const newActivity2 = ["ElenaMarco","home", "tuesday", "18:00"]
+// //     const path1 = path.resolve(__dirname, '../lib/agenda.json')
+// //     const expUserAct = addActivity(newActivity,path1)  
+// //     it ("works adding activity",()=>{    
+// //         const loadedData = db.load(path1)   
+// //         expect(loadedData).toEqual({ElenaMarco:{username:"ElenaMarco", name:"Elena", lastName:"Marco", email: "email", phone: "phone", activityList: ["work", "monday", "9:00"]}})
         
-    })
+// //     })
 
-})
-describe ("List of users function", ()=>{
-    it("works for simpler case", ()=>{
-        const path1 = path.resolve(__dirname, '../lib/agenda.json')
-        const list = listUsers(path1)
-        expect(list[0].username).toEqual("ElenaMarco")
-    })
+// //     it ("works when the activity already exists",()=>{
+// //         const expNewAct = addActivity(newActivity, path1)
+// //         const loadedData = db.load(path1)
+// //         expect(loadedData).toEqual({ElenaMarco:{username: "ElenaMarco",name:"Elena", lastName:"Marco", email: "email", phone: "phone", activityList: ["work", "monday", "9:00"]}})
+
+// //     })
+// //     it ("works when adding another activity",()=>{
+// //         const expnewAct = addActivity(newActivity2, path1)
+// //         const loadedData = db.load(path1)
+// //         expect(loadedData[newActivity[0]].activityList).toEqual(["work", "monday", "9:00","home", "tuesday", "18:00"])
+// //     })
+
+// // })
+// describe ("Edit user function", ()=>{
+//     const editedUser = ["ElenaMarco", "Elena2", "Marco", "email", "phone"]
+//     const editedUser2 = ["ElenaMarco2", "Elena2", "Marco", "email", "phone"]
+//     const path1 = path.resolve(__dirname, '../lib/agenda.json')
+//     console.log(path1)
+//     const loadedData = db.load(path1)
+//     it ("works modifying one element when there is an activity", ()=>{
+//         newuser = editUser(editedUser, path1)
+//         expect(loadedData[editedUser[0]].name).toEqual("Elena2")
+            
+//     })
+//     it("works modifying one element when there is no activity",()=>{
+//         newuser = editUser(editedUser2, path1)
+//         expect(loadedData[editedUser2[0]]).toEqual({"username":"ElenaMarco2","name":"Elena2","lastName":"Marco","email":"email","phone":"phone"})
+//     })
+// })
+// describe("Delete user function",()=>{
+//     const username = "ElenaMarco3"
+//     const path1 = path.resolve(__dirname, '../lib/agenda.json')
     
+//     const loadedData = db.load(path1)
+    
+//     it("works eliminating user",()=>{
+//         deleteUser(username,path1)
+//         expect(loadedData[username]).toEqual(undefined)
+        
+//     })
 
-})
-// describe ("Edit activity function", ()=>{
+// })
+// describe ("List of users function", ()=>{
 //     it("works for simpler case", ()=>{
 //         const path1 = path.resolve(__dirname, '../lib/agenda.json')
-//     activityName = "work"
-//     modifications = ["today", "3"]
-//     modifiedActivity = editActivity("ElenaMarco", activityName, modifications, path1)
-//     expect(modifiedActivity[2]).toEqual("3")
+//         const list = listUsers(path1)
+//         expect(list[0].username).toEqual("ElenaMarco")
 //     })
     
+
 // })
-describe ("Delete activity function", ()=>{
-    it("works for simpler case", ()=>{
-        const path1 = path.resolve(__dirname, '../lib/agenda.json')
-        activityName = "work"
-        username = "ElenaMarco"
-        deleteActivity(username, activityName, path1)
-        loadedData=db.load(path1)
-        expect(loadedData[username].activityList).toEqual(["home","tuesday","18:00"])
-    })
-})
+// // describe ("Edit activity function", ()=>{
+// //     it("works for simpler case", ()=>{
+// //         const path1 = path.resolve(__dirname, '../lib/agenda.json')
+// //     activityName = "work"
+// //     modifications = ["today", "3"]
+// //     modifiedActivity = editActivity("ElenaMarco", activityName, modifications, path1)
+// //     expect(modifiedActivity[2]).toEqual("3")
+// //     })
+    
+// // })
+// describe ("Delete activity function", ()=>{
+//     it("works for simpler case", ()=>{
+//         const path1 = path.resolve(__dirname, '../lib/agenda.json')
+//         activityName = "work"
+//         username = "ElenaMarco"
+//         deleteActivity(username, activityName, path1)
+//         loadedData=db.load(path1)
+//         expect(loadedData[username].activityList).toEqual(["home","tuesday","18:00"])
+//     })
+// })
     
--- test/db.test.js
+++ test/db.test.js
@@ -1,23 +1,83 @@
 const db = require("../lib/db.js")
-const {addUser} = require("../lib/agenda.js")
+db.databaseName = 'agendadb_test'
+const {saveUser, listUsers, addActivity, readUser, readUserActivities, updateUser, editActivity, deleteActivity, deleteUser} = require("../lib/agenda.js")
 
 const path = require('path')
 
-describe("db function",()=>{
-    user = addUser("username", "name", "apellidos", "email","phone")
-    it ("Load file function",()=>{
-     const data=db.load('')
-     expect(data).toEqual({})
-    })
-    it ("works with existing file",()=>{
-        const loadedData= db.load(path.resolve(__dirname, '../lib/dbtest2.json'))
-        expect(loadedData.username).not.toBe(undefined)
-    })
-    it ("writes user on empty file",()=>{
-        const path1 = path.resolve(__dirname, '../lib/dbtest2.json')
-        const dataRead = db.load(path1)
-        db.write(user,path1)
-        const dataRead2 = db.load(path1)
-        expect(dataRead).toEqual(user)
+beforeEach(()=>{
+    return db.startTransaction()
+})
+
+afterEach(()=>{
+    return db.rollbackTransaction().then(()=>{
+        return db.connectionEnd()
     })
 })
+
+describe("db function",()=>{
+    it('works adding user', ()=>{
+        const newUser = ['Elena1', 'Elena', 'Marco', 'email', 'phone']
+        return saveUser(newUser).then((queryResult)=>{
+            db.executeQuery('SELECT * FROM users WHERE username="Elena1"').then((queryResult)=>{
+                expect(queryResult.length).toBe(1)
+            })
+        })
+    })
+    it ('works listing users', ()=>{
+        return listUsers().then((queryResult)=>{
+            expect(queryResult.length).toBe(2)
+        })
+    })
+    it ("works reading one user", ()=>{
+        return readUser('Calu').then((queryResult)=>{
+            db.executeQuery('SELECT * FROM users WHERE username="Calu"').then((queryResult)=>{
+                expect(queryResult[0].surname).toEqual('marco')
+            })
+        })
+    })
+    it ("works adding activity", ()=>{
+        const newAct = ["Calu", "home", "tuesday", "2"]
+        return addActivity(newAct).then((queryResult)=>{
+            db.executeQuery('SELECT * FROM activities WHERE user = "Calu"').then((queryResult)=>{
+                expect(queryResult[1].activity_name).toEqual('home')
+            })
+        })
+
+     })
+    it ("works reading activities", ()=>{
+        return readUserActivities("Calu").then((queryResult)=>{
+            expect(queryResult[0].activity_name).toEqual('work')
+        })
+    })
+    it ("works updating user", ()=>{
+        newData = {surname : 'garcia', phone : 'phone2'}
+        return updateUser("Calu", newData).then((queryResult)=>{
+            db.executeQuery('SELECT * FROM users WHERE username="Calu"').then((queryResult)=>{
+                expect(queryResult[0].phone).toEqual('phone2')
+            })
+        })
+    })
+    it ("works updating activity", ()=>{
+        newData = {activity_hour: '10'}
+        return editActivity("Calu", "work", newData).then((queryResult)=>{
+            db.executeQuery('SELECT * FROM activities WHERE user = "Calu" AND activity_name = "work"').then((queryResult)=>{
+                expect(queryResult[0].activity_hour).toEqual('10')
+            })
+        })
+    })    
+    it ("works deleting activity", ()=>{
+        return deleteActivity("Calu", "gym").then((queryResult)=>{
+            readUserActivities("Calu").then((queryResult)=>{
+                expect(queryResult.length).toEqual(1)
+            })
+        })
+    })
+    it("works deleting user", ()=>{
+        return deleteUser("Elen").then((queryResult)=>{
+            db.executeQuery('SELECT * FROM users WHERE username = "Elen"').then((queryResult)=>{
+                expect(queryResult).toThrow(error) //medir que lance un error
+            
+        })
+    })
+    })
+    })
